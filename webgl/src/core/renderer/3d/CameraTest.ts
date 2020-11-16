7bbe21b7e9540070c0eac9e280f4a737dcb39a1c
hzj <1726987214Qqq.com>
2020-11-16 21:44:29 +0800

    0012

--- webgl/src/core/renderer/3d/CameraTest.ts
+++ webgl/src/core/renderer/3d/CameraTest.ts
@@ -164,11 +164,14 @@ class CameraModel {
       1, -1, 1,
       -1, 1, 1,
       1, 1, 1,
+      0,0,-1,//后节点 8
+      0,0,1 //前节点 9
     ];
     const indices = [
       0, 1, 1, 3, 3, 2, 2, 0, // cube indices
       4, 5, 5, 7, 7, 6, 6, 4,
       0, 4, 1, 5, 3, 7, 2, 6,
+      8,9,9,8
     ];
     return G_ShaderFactory.createBufferInfoFromArrays({
       position: positions,
