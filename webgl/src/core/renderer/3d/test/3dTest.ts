aed27e67d9bb0ae7d3fc31f1b58ff59a68a56b9d
hzj <1726987214@qq.com>
2021-07-05 16:56:57 +0800

    回滚区块

--- webgl/src/core/renderer/3d/test/3dTest.ts
+++ webgl/src/core/renderer/3d/test/3dTest.ts
@@ -66,6 +66,14 @@ function main() {
         return "dog dog dog"
     }
 
+    function cjzRiBen4(){
+        return "dog dog dog"
+    }
+
+    function cjzRiBen5(){
+        return "dog dog dog"
+    }
+
     var cameraAngleRadians = degToRad(0);
     var fieldOfViewRadians = degToRad(60);
     var cameraHeight = 50;
