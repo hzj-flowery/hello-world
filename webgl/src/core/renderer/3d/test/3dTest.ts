a7cbe2b661dbaf75252914f860fa5cfa89f4de70
hzj <1726987214@qq.com>
2021-07-05 16:22:34 +0800

    4

--- webgl/src/core/renderer/3d/test/3dTest.ts
+++ webgl/src/core/renderer/3d/test/3dTest.ts
@@ -70,6 +70,10 @@ function main() {
         return "dog dog dog"
     }
 
+    function cjzRiBen5(){
+        return "dog dog dog"
+    }
+
     var cameraAngleRadians = degToRad(0);
     var fieldOfViewRadians = degToRad(60);
     var cameraHeight = 50;
